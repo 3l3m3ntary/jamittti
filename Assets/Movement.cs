866d9ef484b006bddbb1e2000cbca264b49f0777
Sihvo Teemu TTK19SP <teemusihvo@kamk.fi>
2020-02-01 14:36:27 +0200

    shit

---
+++ Assets/Movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    Vector3 direction;
+
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        GetInput();
+        rb.AddForce(direction * speed);
+        
+    }
+
+    void GetInput()
+    {
+        if (Input.GetKeyDown("d"))
+        {
+            direction = Vector3.forward;
+        }
+        else if(Input.GetKeyUp("d"))
+        {
+           // rb.velocity = Vector3.zero;
+        }
+        else if (Input.GetKeyDown("a"))
+        {
+           direction = Vector3.back;
+        }
+        else if (Input.GetKeyUp("a"))
+        {
+           // rb.velocity = Vector3.zero;
+        }
+    }
+}
