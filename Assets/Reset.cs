ca7f6e51e0e5b0957057685e66a8539557a26bf7
Sihvo Teemu TTK19SP <teemusihvo@kamk.fi>
2020-02-01 15:51:21 +0200

    lick lick lick my ballz

---
+++ Assets/Reset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+    public GameObject ball;
+    
+
+    void Update()
+    {
+     
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        ball.transform.position = new Vector3(40, 25, 0);
+    }
+}
