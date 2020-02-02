05baf98daa43ad3bd82a06ec618003276f80bbf0
Sihvo Teemu TTK19SP <teemusihvo@kamk.fi>
2020-02-02 14:15:39 +0200

    kikkelis mokkelis

---
+++ Assets/ComicShowdown.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComicShowdown : MonoBehaviour
+{
+
+    public GameObject Vomit1;
+   
+    public GameObject Vomit2;
+
+    public GameObject Vomit3;
+   
+    public GameObject Vomit4;
+
+    public GameObject ballero;
+
+    //[HideInInspector]
+    //public bool comic1, comic2, comic3, comic4 = false;
+
+    private void Update()
+    {
+        /*comic1 = GetComponent<Movement>().c1;
+        comic2 = GetComponent<Movement>().c2;
+        comic3 = GetComponent<Movement>().c3;
+        comic4 = GetComponent<Movement>().c4;*/
+        vomitCheck();
+    }
+    void vomitCheck()
+    {
+        /*if(comic1 == true)
+        {
+            Comic1.SetActive(true);
+        }
+        if (comic2 == true)
+        {
+            Comic2.SetActive(true);
+        }
+        if (comic3 == true)
+        {
+            Comic3.SetActive(true);
+        }
+        if (comic4 == true)
+        {
+            Comic4.SetActive(true);
+        }*/
+        if (ballero.GetComponent<Movement>().Comic1 != null)
+        {
+            Vomit1.SetActive(true);
+        }
+        if (ballero.GetComponent<Movement>().Comic2 != null)
+        {
+            Vomit2.SetActive(true);
+        }
+        if (ballero.GetComponent<Movement>().Comic3 != null)
+        {
+            Vomit3.SetActive(true);
+        }
+        if (ballero.GetComponent<Movement>().Comic4 != null)
+        {
+            Vomit4.SetActive(true);
+        }
+    }
+}
--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -7,6 +7,8 @@ public class Movement : MonoBehaviour
     private Rigidbody rb;
     public float speed;
     Vector3 direction;
+    //public bool c1, c2, c3, c4 = false;
+
 
     public GameObject Comic1, Comic2, Comic3, Comic4;
 
@@ -43,21 +45,25 @@ public class Movement : MonoBehaviour
         {
             Comic1 = collision.gameObject;
             collision.gameObject.SetActive(false);
+            //c1 = true;
         }
         if (collision.gameObject.CompareTag("Comic2"))
         {
             Comic2 = collision.gameObject;
             collision.gameObject.SetActive(false);
+            //c2 = true;
         }
         if (collision.gameObject.CompareTag("Comic3"))
         {
             Comic3 = collision.gameObject;
             collision.gameObject.SetActive(false);
+            //c3 = true;
         }
         if (collision.gameObject.CompareTag("Comic4"))
         {
             Comic4 = collision.gameObject;
             collision.gameObject.SetActive(false);
+            //c4 = true;
         }
     }
 }
